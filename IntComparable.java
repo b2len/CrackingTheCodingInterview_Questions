8744ad39e28244ef1ba95cf4fd165a8d67669fc3
unknown <zyi.commerce@outlook.com>
2022-01-31 14:16:01 -0700

    Sorting and Searching 10.9 - 10.10

---
+++ IntComparable.java
@@ -0,0 +1,11 @@
+package SortingAndSearching;
+
+import java.util.Comparator;
+
+public class IntComparable implements Comparator<Integer>{
+
+    @Override
+    public int compare(Integer o1, Integer o2) {
+        return o1.compareTo(o2);
+    }
+}
