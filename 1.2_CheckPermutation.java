18a92a2540da0dceffc82d4ddb73a205ab14ff91
unknown <zyi.commerce@outlook.com>
2022-07-08 10:03:37 -0600

    1.2

--- 1.2_CheckPermutation.java
+++ 1.2_CheckPermutation.java
@@ -1,14 +1,24 @@
 package ArraysAndStrings;
 
-/** Given two strings, write a method to decide if one is a permutation of the other?
- *  Clarification with Interviewer: is it case sensitive? is it whitespace sensitive?
+/**
+ * Given two strings, write a method to decide if one is a permutation of the
+ * other?
+ * Clarification with Interviewer: is it case sensitive? is it whitespace
+ * sensitive?
+ * 
+ * July 08, 2022: be able to formulate the right solution but failed to write
+ * the correct code
+ * Main issue is not familiar with String char related operation,
+ * especially String.CharAt() and String.toCharArray()
  */
-
 
 public class CheckPermutation {
 
-    /** Solution 1: if the two strings are permutations to each other, this means they are
-     * the same length with different arrangement of characters. Thus, if we sort the strings,
+    /**
+     * Solution 1: if the two strings are permutations to each other, this means
+     * they are
+     * the same length with different arrangement of characters. Thus, if we sort
+     * the strings,
      * the result should be the same
      */
 
@@ -26,7 +36,7 @@ public class CheckPermutation {
     }
 
     public static void main(String[] args) {
-        String[][] pairs = {{"apple", "papel"}, {"carrot", "tarroc"}, {"hello", "llloh"}};
+        String[][] pairs = { { "apple", "papel" }, { "carrot", "tarroc" }, { "hello", "llloh" } };
         for (String[] pair : pairs) {
             String word1 = pair[0];
             String word2 = pair[1];
@@ -35,23 +45,28 @@ public class CheckPermutation {
         }
     }
 
-    /** Solution 2: Use the definition of a permutation - two words with the same character counts
-     * we can iterate through counting how many times each character appears, then compare the two arrays
-     * 	public static boolean permutation(String s, String t) {
-     * 		if (s.length() != t.length()) return false; // Permutations must be same length
+    /**
+     * Solution 2: Use the definition of a permutation - two words with the same
+     * character counts
+     * we can iterate through counting how many times each character appears, then
+     * compare the two arrays
+     * public static boolean permutation(String s, String t) {
+     * if (s.length() != t.length()) return false; // Permutations must be same
+     * length
      *
-     * 		int[] letters = new int[128]; // Assumption: ASCII
-     * 		for (int i = 0; i < s.length(); i++) {
-     * 			letters[s.charAt(i)]++;
-     *                }
+     * int[] letters = new int[128]; // Assumption: ASCII
+     * for (int i = 0; i < s.length(); i++) {
+     * letters[s.charAt(i)]++;
+     * }
      *
-     * 		for (int i = 0; i < t.length(); i++) {
-     * 			letters[t.charAt(i)]--;
-     * 		    if (letters[t.charAt(i)] < 0) {
-     * 		    	return false;
-     *            }
-     *        }
+     * for (int i = 0; i < t.length(); i++) {
+     * letters[t.charAt(i)]--;
+     * if (letters[t.charAt(i)] < 0) {
+     * return false;
+     * }
+     * }
      *
-     * 		return true; // letters array has no negative values, and therefore no positive values either
+     * return true; // letters array has no negative values, and therefore no
+     * positive values either
      */
 }
